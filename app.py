33dce622cac0e4ca7ca2f17405b68f7fa23eae8d
Adarshreddy adelli <adarshreddy9849@gmail.com>
2019-02-27 21:00:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
 from flask import Flask ,render_template
-from flask_nav.elements import Navbar, View
-from flask_nav import Nav
-from flask_nav.elements import Navbar, View
-
-
-nav = Nav()
 
 app = Flask(__name__)
-nav.init_app(app)
-
-def user_is_logged_in():
-    return True
-    
-@nav.navigation()
-def top_nav():
-    items = [View('Home', 'index'), View('Shopping Area', 'index')]
-
-    # only logged in users get to see the secret shop
-    if user_is_logged_in():
-        items.append(View('Secret Shop', 'secret'))
-
-    return Navbar('', *items)
 
 @app.route("/")
 def main():
